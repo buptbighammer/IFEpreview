6c43393115089c62d538e4dd36c5a86d2f30382b
buptbighammer <420778382@qq.com>
2017-03-21 00:21:44 +0800

    修改连续点击出现混乱及报错的bug

--- 2017/binbin/task7/task7.js
+++ 2017/binbin/task7/task7.js
@@ -1,52 +1,67 @@
-function backGroundAction(arr){
-	for(var i=0; i<arr.length;i++){
-		setTimeout(function(i){
-			return function(){
-				arr[i].style.background = "blue";
-			}
-		}(i),i*500);
-		setTimeout(function(i){
-			return function(){
-				arr[i].style.background = "";
-			}
-		}(i),i*500+499);
+var arr = [];
+var time;
+function reset(){
+	clearInterval(time);
+	var nodeList = document.getElementsByTagName("div");
+	for(var i = 0; i<nodeList.length;i++){
+		nodeList[i].style.background = "";
 	}
+	arr=[];
+}
+function backGroundAction(){
+	arr[0].style.background = "blue";
+	var i=0;
+	var len = arr.length;
+	time = setInterval(function(){
+		i++;
+		if(i<len){
+			arr[i-1].style.background = "";
+			arr[i].style.background = "blue";
+		}
+		else{
+			arr[len-1].style.background = "";
+			clearInterval(time);
+			arr= [];
+		}
+	},500);
+	
 }
 //nodeList默认为前序遍历，将nodeList转为数组
 function preOrder(){
+	reset();
 	var nodeList = document.getElementsByTagName("div");
-	var arr = Array.prototype.slice.call(nodeList,0);
-	backGroundAction(arr);
+	arr = Array.prototype.slice.call(nodeList,0);
+	backGroundAction();
 }
 //中序遍历迭代，将左子节点迭代，父节点入数组，右子节点迭代
-function _order(node,arr){
+function _order(node){
 	if(node){
-		_order(node.firstElementChild,arr);
+		_order(node.firstElementChild);
 		arr.push(node);
-		_order(node.lastElementChild,arr);
+		_order(node.lastElementChild);
 	}
 }
 //迭代的方法中序遍历
 function order(){
+	reset();
 	var nodeList = document.getElementsByTagName("div");
-	var arr = new Array();
-	_order(nodeList[0],arr);
-	backGroundAction(arr);
+	_order(nodeList[0]);
+	backGroundAction();
 }
 //后续遍历迭代，将左子节点迭代，右子节点迭代，父节点入数组
-function _posterior(node,arr){
+function _posterior(node){
 	if(node){
-		_posterior(node.firstElementChild,arr);
-		_posterior(node.lastElementChild,arr);
+		_posterior(node.firstElementChild);
+		_posterior(node.lastElementChild);
 		arr.push(node);
 	}
 }
 //迭代的方法后续遍历
 function posterior(){
+	reset();
 	var nodeList = document.getElementsByTagName("div");
-	var arr = new Array();
-	_posterior(nodeList[0],arr);
-	backGroundAction(arr);
+	_posterior(nodeList[0]);
+	backGroundAction();
 }
 function addOnclickEvent(){
 	document.getElementById("pre").onclick = preOrder;
